b72dfe4ec5ab7b6097f2364a4b13989430c0a240
Linzi <linzi-s@outlook.com>
2024-11-24 21:58:12 +0800

    Add JSX rendering and useState hook practice template
    Included a basic template for the practice project focused on JSX rendering and the useState hook to help get started quickly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,154 @@
-import React, { useState } from "react";
-import "./index.css";
+import "./App.scss";
+import avatar from "./images/winnie.png";
 
-const count: number = 100;
+/**
+ * 评论列表的渲染和操作
+ *
+ * 1. 根据状态渲染评论列表
+ * 2. 删除评论
+ */
 
-const sayHi = () => {
-  return "Hi~";
+/**
+ * 评论列表数据:
+ * 1. 使用 useState 维护评论列表
+ */
+const defaultList = [
+  {
+    // 评论id
+    rpid: 3,
+    // 用户信息
+    user: {
+      uid: '13258165',
+      avatar: '',
+      uname: '周杰伦',
+    },
+    // 评论内容
+    content: '哎哟，不错哦',
+    // 评论时间
+    ctime: '10-18 08:15',
+    like: 88,
+  },
+  {
+    rpid: 2,
+    user: {
+      uid: '36080105',
+      avatar: '',
+      uname: '许嵩',
+    },
+    content: '我寻你千百度 日出到迟暮',
+    ctime: '11-13 11:29',
+    like: 88,
+  },
+  {
+    rpid: 1,
+    user: {
+      uid: '30009257',
+      avatar,
+      uname: '黑马前端',
+    },
+    content: '学前端就来黑马',
+    ctime: '10-19 09:00',
+    like: 66,
+  },
+];
+// 当前登录用户信息
+const user = {
+  // 用户id
+  uid: '30009257',
+  // 用户头像
+  avatar,
+  // 用户昵称
+  uname: '黑马前端',
 };
-
-const items = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-  { id: 3, name: "Charlie" },
+/**
+ * 导航 Tab 的渲染和操作
+ *
+ * 1. 渲染导航 Tab 和高亮
+ * 2. 评论列表排序
+ *  最热 => 喜欢数量降序
+ *  最新 => 创建时间降序
+ */
+// 导航 Tab 数组
+const tabs = [
+  {type: 'hot', text: '最热'},
+  {type: 'time', text: '最新'},
 ];
 
-const loggedIn: boolean = false;
-
-const style = {
-  color: "pink",
-  fontSize: "larger",
-};
-
-function ButtonComponent() {
-  // 1. 调用 useState 添加一个状态变量：
-  const [count, setCount] = useState(0);
-  // 2. 点击事件回调：
-  const handleClick = () => {
-    // 作用1：用传入的新的值 count + 1 修改原有的 count 值。
-    // 作用2：重新使用新的 count 的值渲染按钮 UI。
-    setCount(count + 1);
-  };
-  return <button onClick={handleClick}>{count}</button>;
-}
-
-const handleClick = (name: any, event: any) => {
-  console.log("clicked! event->", name, event);
-};
-
 function App() {
-  const [form, setForm] = useState({ name: "linzi" });
-  const changeForm = () => {
-    setForm({ ...form, name: "Linzi2" });
-  };
   return (
-    <div className="App">
-      This is a streamlined React starter project.
-      {/*使用引号传递字符串*/}
-      <div style={{ color: "red" }}>{"this is a message"}</div>
-      {/*使用 JavaScript 变量*/}
-      {count}
-      {/*函数调用*/}
-      <div>{sayHi()}</div>
-      {/*方法调用*/}
-      <div>{new Date().getDate()}</div>
-      {/*使用 JavaScript 对象*/}
-      <div className="foo">This is a div</div>
-      <div style={style}>This is a div</div>
-      {/* 渲染列表 */}
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            {item.id}---{item.name}
+    <div className="app">
+      {/* 导航 Tab */}
+      <div className="reply-navigation">
+        <ul className="nav-bar">
+          <li className="nav-title">
+            <span className="nav-title-text">评论</span>
+            {/* 评论数量 */}
+            <span className="total-reply">{10}</span>
           </li>
-        ))}
-      </ul>
-      <div>
-        {/* 逻辑与 &&*/}
-        {loggedIn && <span>logged in!</span>}
-        {/* 三元运算符 */}
-        {loggedIn ? <span>logged in!</span> : <span>IDLE</span>}
+          <li className="nav-sort">
+            {/* 高亮类名： active */}
+            <span className="nav-item">最新</span>
+            <span className="nav-item">最热</span>
+          </li>
+        </ul>
       </div>
-      <div>
-        <button onClick={(e) => handleClick("Linzi", e)}>click here!</button>
-      </div>
-      <ButtonComponent />
-      <ButtonComponent></ButtonComponent>
-      {/* 修改对象的状态 */}
-      <div>
-        <button onClick={changeForm}>change form: {form.name}</button>
+
+      <div className="reply-wrap">
+        {/* 发表评论 */}
+        <div className="box-normal">
+          {/* 当前用户头像 */}
+          <div className="reply-box-avatar">
+            <div className="bili-avatar">
+              <img className="bili-avatar-img" src={avatar} alt="用户头像"/>
+            </div>
+          </div>
+          <div className="reply-box-wrap">
+            {/* 评论框 */}
+            <textarea
+              className="reply-box-textarea"
+              placeholder="发一条友善的评论"
+            />
+            {/* 发布按钮 */}
+            <div className="reply-box-send">
+              <div className="send-text">发布</div>
+            </div>
+          </div>
+        </div>
+        {/* 评论列表 */}
+        <div className="reply-list">
+          {/* 评论项 */}
+          <div className="reply-item">
+            {/* 头像 */}
+            <div className="root-reply-avatar">
+              <div className="bili-avatar">
+                <img
+                  className="bili-avatar-img"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            <div className="content-wrap">
+              {/* 用户名 */}
+              <div className="user-info">
+                <div className="user-name">jack</div>
+              </div>
+              {/* 评论内容 */}
+              <div className="root-reply">
+                <span className="reply-content">这是一条评论回复</span>
+                <div className="reply-info">
+                  {/* 评论时间 */}
+                  <span className="reply-time">{'2023-11-11'}</span>
+                  {/* 评论数量 */}
+                  <span className="reply-time">点赞数:{100}</span>
+                  <span className="delete-btn">
+                    删除
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
